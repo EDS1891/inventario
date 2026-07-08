f21c0f763c0bc6c4689aa85fcfc0f22e96902c0f
Emiliano Dominguez <edominguez@capenarol.com.uy>
2026-07-08 12:56:33 -0300

    Email: cambiar footer a ENTREGAS INDUMENTARIA PEÑAROL

--- api/send-email.js
+++ api/send-email.js
@@ -56,7 +56,7 @@ export default async function handler(req, res) {
           </a>
         </div>
         <div style="padding:16px 24px;background:#f0f0f0;font-size:12px;color:#888;text-align:center">
-          Depósito Peñarol &nbsp;·&nbsp; edominguez@capenarol.com.uy
+          ENTREGAS INDUMENTARIA PEÑAROL
         </div>
       </div>
     `
